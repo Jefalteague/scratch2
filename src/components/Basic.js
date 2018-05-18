9cf6c9e63663c0af8762e6fd72f427e9dd64cc0e
Jeffrey Teague <jefalteague@gmail.com>
2018-05-18 07:05:28 -0400

    Updates to Basic and css.

--- src/components/Basic.js
+++ src/components/Basic.js
@@ -5,7 +5,15 @@ class Basic extends Component {
   render() {
     return(
       <div>
-        This will be the basic information about the browse functionality.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet metus eget ante rutrum varius sit amet ac purus. Mauris et fermentum dolor. Vivamus est enim, tincidunt nec arcu vel, venenatis accumsan sapien. Donec sagittis leo a leo consequat pharetra eget sed dolor. Ut arcu nisl, tristique id lorem at, rutrum dictum diam. Suspendisse tempus ut arcu at sollicitudin. In hac habitasse platea dictumst. Duis elementum pellentesque sapien, non auctor eros. Pellentesque scelerisque vehicula neque quis pharetra. Maecenas et varius felis, vel euismod felis. Ut odio sem, ullamcorper ac enim in, malesuada posuere purus. Suspendisse rutrum quam at elit porta, eu vestibulum elit sollicitudin. Fusce tincidunt facilisis diam et viverra. Sed dictum sem velit, sit amet vehicula ipsum feugiat a. Nam sed finibus metus.
+
+      Mauris id commodo justo, sit amet porttitor leo. Quisque neque dolor, mollis sed justo lacinia, molestie dictum orci. Maecenas eros mi, dictum at lectus eget, congue facilisis nisi. Integer blandit sed mi in consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce sed vehicula ante. Praesent vehicula ex quis tincidunt venenatis. Ut sagittis mollis faucibus. Suspendisse nisi metus, cursus a nisi bibendum, ultricies finibus dui. Donec aliquet aliquam tortor sed scelerisque. Phasellus sed odio commodo, posuere magna nec, maximus ligula.
+
+      Maecenas tempus ipsum ac sem faucibus ultricies. Nam facilisis arcu nec nulla tincidunt facilisis. Maecenas aliquam ultrices nisl id lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque semper felis sit amet vulputate fermentum. Proin consectetur consequat semper. Suspendisse potenti. Morbi pulvinar imperdiet feugiat. Fusce vitae nisi sagittis, sagittis ipsum et, porta lacus. Donec suscipit, libero sed gravida ullamcorper, justo nibh consequat justo, at facilisis lectus dui eu ligula. Maecenas neque tellus, rutrum ac dignissim a, consectetur at mi. In vel sollicitudin lorem. Integer erat massa, eleifend ut eros id, interdum finibus mauris.
+
+      Nullam non elit sapien. Ut varius tortor quam, at molestie nibh sollicitudin vel. Nulla tempor urna in varius placerat. Cras quis neque a orci aliquet molestie. Nulla ut aliquet turpis. Donec non imperdiet urna. Quisque tincidunt nibh tortor, vel fermentum justo ultrices quis. Nulla consectetur odio vitae turpis ornare, at faucibus diam vehicula. Nam nisl magna, pharetra vel libero vel, tempus fermentum eros. Pellentesque sodales nibh nunc, eu placerat velit tincidunt eu. Proin id ex fermentum, venenatis nisl nec, sagittis tortor. Nam malesuada sollicitudin tortor eu luctus. Morbi faucibus tincidunt risus, at porta turpis bibendum sit amet.
+
+
       </div>
     );
   }
